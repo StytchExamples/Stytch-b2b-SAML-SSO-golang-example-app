bf02fd4558060786988d1c7cd21ce1091144a7f5
6lvcking <connectejehisaac@gmail.com>
2024-08-02 15:30:35 +0100

    Removed config directory containing old Postgres connection and made chnages to the database directory on the server

--- server/internal/database/database.go
+++ server/internal/database/database.go
@@ -17,18 +17,5 @@ func Connect() (*gorm.DB, error) {
 	}
 	db.AutoMigrate(&models.Tenant{}, &models.Member{})
 
-	fmt.Println(dbPath)
-
-	fmt.Println(err)
-
-	// SetMaxIdleConns sets the maximum number of connections in the idle connection pool.
-	// sqlDB.SetMaxIdleConns(10)
-
-	// // SetMaxOpenConns sets the maximum number of open connections to the database.
-	// sqlDB.SetMaxOpenConns(100)
-
-	// // SetConnMaxLifetime sets the maximum amount of time a connection may be reused.
-	// sqlDB.SetConnMaxLifetime(time.Hour)
-
 	return db, nil
 }
