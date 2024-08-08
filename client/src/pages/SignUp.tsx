20649a5cc5ecf17620a3ebb8c552c5e10b33fbee
6lvcking <connectejehisaac@gmail.com>
2024-08-08 11:09:06 +0100

    update character length for name fields

--- client/src/pages/SignUp.tsx
+++ client/src/pages/SignUp.tsx
@@ -10,8 +10,8 @@ import { axiosInstance, excludedProviders, useMagicLinkSignIn } from "../utils";
 
 const schema = z.object({
   company_name: z.string().min(3, "Company name is required"),
-  first_name: z.string().min(5, "First name is required"),
-  last_name: z.string().min(5, "Last name is required"),
+  first_name: z.string().min(2, "First name is required"),
+  last_name: z.string().min(2, "Last name is required"),
   email: z
     .string()
     .email("Email is required")
