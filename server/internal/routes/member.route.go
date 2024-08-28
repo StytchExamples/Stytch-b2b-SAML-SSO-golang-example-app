0aae19dafdf02dd8522a64c832a7d51b79ae7531
6lvcking <connectejehisaac@gmail.com>
2024-08-28 23:37:07 +0100

    Removed unnecessary comments

--- server/internal/routes/member.route.go
+++ server/internal/routes/member.route.go
@@ -7,7 +7,6 @@ import (
 	"saml_sso/internal/services"
 )
 
-// MemberRoutes sets up the member-related routes
 func MemberRoutes(r *gin.RouterGroup, db *gorm.DB) {
 	r.GET("/profile/:stytch_member_id", func(c *gin.Context) {
 		services.GetMemberProfile(c, db)
